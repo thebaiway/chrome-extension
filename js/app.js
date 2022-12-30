ab33c0a29684bb5cfcaac56da4d12144f1176b1f
Bailee <bailee.roby@gmail.com>
2022-12-29 20:29:55 -0800

    cleaning up my code

--- js/app.js
+++ js/app.js
@@ -1,21 +1,34 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
-    
+    renderLeads()
 })
 
-for (let i = 0; i < myLeads.length; i++) {
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
 
-    // alternative way to do this: 
+function renderLeads() {
+  let listItems = ""
 
-    // const li = document.createElement("li")
-    // li.textContent = myLeads[i]
-    // ulEl.append(li)
-
+  for (let i = 0; i < myLeads.length; i++) {
+  listItems += "<li>" + myLeads[i] + "</li>"
+  
+  ulEl.innerHTML = listItems
+ }
 }
+
+
+
+
+// Note to self:
+
+//  ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+
+//  alternative way to do this: 
+
+// const li = document.createElement("li")
+// li.textContent = myLeads[i]
+// ulEl.append(li) 
+
