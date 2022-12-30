e4992e5c813a100e762f9bcde8011ba76ac64c20
Bailee <bailee.roby@gmail.com>
2022-12-29 19:43:35 -0800

    working on logic

---
+++ js/app.js
@@ -0,0 +1,21 @@
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function () {
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+    
+})
+
+for (let i = 0; i < myLeads.length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+
+    // alternative way to do this: 
+
+    // const li = document.createElement("li")
+    // li.textContent = myLeads[i]
+    // ulEl.append(li)
+
+}
