e0f78ef8b31f8c0fc6b12727c5d22e0aa01dcb1e
Bailee <bailee.roby@gmail.com>
2023-01-05 18:35:30 -0800

    finishing up chrome extension

--- js/app.js
+++ js/app.js
@@ -11,27 +11,14 @@ if (leadsFromLocalStorage) {
   render(myLeads)
 }
 
-const tabs = [
-  {url: "https://www.linkedin.com/in/per-harald-borgen/"}
-]
 
 tabBtn.addEventListener("click", function () {
-  // console.log(tabs[0].url)
 
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-
-    // since only one tab should be active and in the current window at once
-    // the return variable should only have one entry
-    let activeTab = tabs[0]
-    let activeTabId = activeTab.id // or do whatever you need
-
- });
-
-  myLeads.push(tabs[0].url)
-  inputEl.value = ""
-  localStorage.setItem("myLeads", JSON.stringify(myLeads))
-  render(myLeads)
-
+  chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+  })
 })
 
 function render(leads) {
